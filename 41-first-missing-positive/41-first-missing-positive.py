ad94cdc5dc9b1a892543ae57b098e83ad184a1f4
austenjs <64929507+austenjs@users.noreply.github.com>
2022-01-05 14:49:29 +0800

    Time: 1570 ms (5.02%), Space: 69.1 MB (12.41%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -1,6 +1,11 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        hashset = set(nums)
-        for i in range(1, max(max(nums) + 2, 2)):
+        hashset = set()
+        max_num = 2
+        for num in nums:
+            if num > 0:
+                hashset.add(num)
+                max_num = max(num, max_num)
+        for i in range(1, max_num + 2):
             if i not in hashset:
                 return i

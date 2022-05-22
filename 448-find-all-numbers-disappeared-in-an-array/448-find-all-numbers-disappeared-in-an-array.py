dab8fd07106e3b5416ff28256aaa31e01c0c4c69
austenjs <64929507+austenjs@users.noreply.github.com>
2022-05-22 16:05:10 +0800

    Time: 1009 ms (5.01%), Space: 22.4 MB (55.22%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
@@ -1,12 +1,29 @@
+from math import ceil, log
+
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         N = len(nums)
-        
-        # O(1) seek
-        existing_nums = set(nums)
-        
+        change = True
+
+        for _ in range(ceil(log(N))):
+            if not change:
+                break
+            change = False
+            i = 0
+            while i < N:
+                num = nums[i]
+                target_index = num - 1
+                if i == target_index:
+                    i += 1
+                    continue
+                tmp = nums[target_index]
+                nums[target_index] = nums[i]
+                nums[i] = tmp
+                i += 1
+                change = True
+
         ans = []
         for i in range(1, N + 1):
-            if i not in existing_nums:
+            if nums[i - 1] != i:
                 ans.append(i)
         return ans

a33679a64c024e21501fe7dbdbcfb30a5ec99ac4
austenjs <64929507+austenjs@users.noreply.github.com>
2022-05-22 15:45:34 +0800

    Time: 243 ms (28.24%), Space: 15.2 MB (77.45%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,11 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         N = len(nums)
-
-        # O(1) seek
-        num_in_nums = set(nums)
         
-        for i in range(0, N + 1):
-            if i not in num_in_nums:
-                return i
-        return -1
+        # Using sum formula
+        return N * (N + 1) // 2 - sum(nums)

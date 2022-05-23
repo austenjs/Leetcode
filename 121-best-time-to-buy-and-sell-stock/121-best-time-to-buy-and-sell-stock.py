7abbc52e24bec62ec5bac0622a34acca380d6829
austenjs <64929507+austenjs@users.noreply.github.com>
2022-05-23 20:57:20 +0800

    Time: 1606 ms (26.12%), Space: 25.1 MB (38.82%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,18 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        left, right = 0, 1
         N = len(prices)
-        maximum = [0 for _ in range(N)]
-        minimum = [1e4 for _ in range(N)]
-        
-        maximum[N - 1] = prices[N - 1]
-        minimum[0] = prices[0]
-        for i in range(N - 2, -1, -1):
-            maximum[i] = max(maximum[i + 1], prices[i])
-        
-        for i in range(1, N):
-            minimum[i] = min(minimum[i - 1], prices[i])
-        
         ans = 0
-        for i in range(N):
-            ans = max(ans, maximum[i] - minimum[i])
+        
+        while right < N:
+            if prices[right] < prices[left]:
+                left = right
+            else:
+                ans = max(ans, prices[right] - prices[left])
+            right += 1
+        
         return ans

51551524adc974d48ad31c1b640b254050a5c3d3
austenjs <64929507+austenjs@users.noreply.github.com>
2022-05-23 21:02:38 +0800

    Time: 774 ms (83.48%), Space: 27.8 MB (79.09%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,9 +1,9 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        ans = -10000
+        ans = nums[0]
         current = 0
         for num in nums:
             current += num
-            ans = max(ans, current)
+            ans = current if current > ans else ans
             current = 0 if current < 0 else current
         return ans

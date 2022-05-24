6f8900a0484f7f10a1caf9c8ab34d56b8046a4b0
austenjs <64929507+austenjs@users.noreply.github.com>
2022-05-24 20:19:45 +0800

    Time: 151 ms (37.64%), Space: 20.7 MB (97.85%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,6 +1,6 @@
 class Solution:
     def __init__(self):
-        self.memo = {0 : 0, 1 : 1, 2 : 1}
+        self.memo = {0 : 0, 1 : 1}
 
     def countBits(self, n: int) -> List[int]:
         ans = []
@@ -11,14 +11,9 @@ class Solution:
     def countBit(self, n):
         if n in self.memo:
             return self.memo[n]
-        ans = 0
-        current = 1
-        while 2 * current <= n:
-            current *= 2
-        while n > 0:
-            if n >= current:
-                n -= current
-                ans += 1
-            current //= 2
+        if n % 2 == 1:
+            ans = self.countBit(n // 2) + 1
+        else:
+            ans = self.countBit(n // 2)
         self.memo[n] = ans
         return ans

2e9514ee94833b035b544a06ff66005be29ab361
austenjs <64929507+austenjs@users.noreply.github.com>
2022-05-29 23:19:42 +0800

    Time: 301 ms (15.38%), Space: 15.4 MB (86.11%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,10 +1,13 @@
-from collections import Counter
+import random
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        N = len(nums)
-        counter = Counter(nums)
-        for key, val in counter.items():
-            if val > N // 2:
-                return key
-        return -1
+        count = 0
+        candidate = None
+        
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if num == candidate else - 1
+        
+        return candidate

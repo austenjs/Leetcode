59ede94a8ba0cafad95d3e4ca114560cb8391bab
austenjs <64929507+austenjs@users.noreply.github.com>
2022-05-29 22:20:58 +0800

    Time: 118 ms (42.82%), Space: 15.2 MB (49.55%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,12 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        number_to_index = {}
+        num_to_index = {}
         for i, num in enumerate(nums):
-            number_to_index[num] = i
+            num_to_index[num] = i
         for i, num in enumerate(nums):
             complement = target - num
-            if complement in number_to_index:
-                complement_index = number_to_index[complement]
-                if complement_index != i:
-                    return [i, complement_index]
+            if complement not in num_to_index:
+                continue
+            index = num_to_index[complement]
+            if index == i:
+                continue
+            return [i, index]
         return [-1, -1]

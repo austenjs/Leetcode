72a14e90af0277def36985616a765e4c07534252
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-01 17:37:27 +0800

    Time: 59 ms (70.94%), Space: 17 MB (14.94%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.py
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.py
@@ -1,11 +1,5 @@
 from collections import defaultdict
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def __init__(self):
         self.count = defaultdict(int)

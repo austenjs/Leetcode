a2daaf89ccdbc115111c77b1e33c0e3c1a85f469
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-01 23:33:51 +0800

    Time: 16 ms (98.41%), Space: 13.5 MB (61.13%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -1,20 +1,24 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
         if list1 is None:
             return list2
-        elif list2 is None:
+        if list2 is None:
             return list1
-
-        if list1.val <= list2.val:
-            merged_list = list1
-            merged_list.next = self.mergeTwoLists(list1.next, list2)
+        
+        if list1.val < list2.val:
+            head = list1
+            head.next = self.mergeTwoLists(list1.next, list2)
         else:
-            merged_list = list2
-            merged_list.next = self.mergeTwoLists(list1, list2.next)
-            
-        return merged_list
+            head = list2
+            head.next = self.mergeTwoLists(list1, list2.next)
+        return head

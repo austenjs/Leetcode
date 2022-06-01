da9224b157b409e46cade387fdeeecec1d4880da
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-01 23:45:24 +0800

    Time: 68 ms (51.41%), Space: 14.4 MB (97.27%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -2,5 +2,4 @@ from collections import Counter
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        count1, count2 = Counter(s), Counter(t)
-        return count1 == count2
+        return Counter(s) == Counter(t)

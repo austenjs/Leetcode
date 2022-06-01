0de9af11f47a99abeadbed4c241e1a2196453e5b
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-01 17:31:46 +0800

    Time: 39 ms (86.23%), Space: 13.9 MB (78.59%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -5,19 +5,16 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if list1 is None and list2 is None:
-            return None
         if list1 is None:
             return list2
-        if list2 is None:
+        elif list2 is None:
             return list1
-        
-        if list1.val > list2.val:
-            combined = self.mergeTwoLists(list1, list2.next)
-            list2.next = combined
-            return list2
-        
+
+        if list1.val <= list2.val:
+            merged_list = list1
+            merged_list.next = self.mergeTwoLists(list1.next, list2)
         else:
-            combined = self.mergeTwoLists(list1.next, list2)
-            list1.next = combined
-            return list1
+            merged_list = list2
+            merged_list.next = self.mergeTwoLists(list1, list2.next)
+            
+        return merged_list

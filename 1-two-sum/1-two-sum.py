efcdbfcb5f1e36d67de2ef1fc2d34073adb4095a
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-01 23:28:05 +0800

    Time: 78 ms (59.76%), Space: 14.6 MB (5.35%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,14 +1,16 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_to_index = {}
-        for i, num in enumerate(nums):
-            num_to_index[num] = i
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        visited = set()
         for i, num in enumerate(nums):
             complement = target - num
-            if complement not in num_to_index:
+            if complement not in visited:
+                visited.add(num)
                 continue
-            index = num_to_index[complement]
-            if index == i:
-                continue
-            return [i, index]
-        return [-1, -1]
+            for j in range(i):
+                if nums[j] == complement:
+                    return [i, j]

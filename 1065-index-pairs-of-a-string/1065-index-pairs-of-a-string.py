4f5a2461a5e7d7c2af8090e9cc4e84a9f829a241
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-05 00:25:01 +0800

    Time: 60 ms (42.74%), Space: 14.2 MB (8.22%) - LeetHub

--- 1065-index-pairs-of-a-string/1065-index-pairs-of-a-string.py
+++ 1065-index-pairs-of-a-string/1065-index-pairs-of-a-string.py
@@ -1,20 +1,34 @@
+class Trie:
+    def __init__(self):
+        self.root = {}
+    
+    def insert(self, word):
+        cur = self.root
+
+        for char in word:
+            if char not in cur:
+                cur[char] = {}
+            cur = cur[char]
+        
+        # * means the end of this word
+        cur['*'] = word
+        
 class Solution:
     def indexPairs(self, text: str, words: List[str]) -> List[List[int]]:
-        ans = []
-        for word in words:
-            ans.extend(self.findWord(text, word))
-        return sorted(ans, key = lambda x: (x[0], x[1]))        
-        
-    def findWord(self, text, word):
-        ans = []
+        trie = Trie()
         N = len(text)
-        n = len(word)
+        result = []
+        
+        for word in words:
+            trie.insert(word)
+        
         for i in range(N):
-            match = True
-            for k in range(n):
-                if i + k == N or text[i + k] != word[k]:
-                    match = False
-                    break
-            if match:
-                ans.append([i, i + n - 1])
-        return ans
+            dic = trie.root
+            k = 0
+            
+            while i + k < N and text[i + k] in dic:
+                dic = dic[text[i + k]]
+                if '*' in dic:
+                    result.append([i, i + k])       
+                k += 1   
+        return result

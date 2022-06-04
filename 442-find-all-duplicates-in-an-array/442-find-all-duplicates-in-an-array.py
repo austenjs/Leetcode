6d28a9f5e7da733956e0e4fcd7a97118c4b463d0
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-05 00:56:20 +0800

    Time: 492 ms (44.89%), Space: 21.9 MB (47.32%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.py
@@ -3,8 +3,9 @@ class Solution:
         ans = []
         
         for num in nums:
-            if nums[abs(num) - 1] < 0:
-                ans.append(abs(num))
-            nums[abs(num) - 1] *= -1
+            num = num if num > 0 else -num
+            if nums[num - 1] < 0:
+                ans.append(num)
+            nums[num - 1] *= -1
         
         return ans

a58994b1e0a48f9ecd9fbd9d0131c787187af7d9
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-05 01:09:59 +0800

    Time: 142 ms (73.14%), Space: 14.6 MB (89.90%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -5,16 +5,33 @@ class Solution:
         """
         num_rows = len(matrix)
         num_cols = len(matrix[0])
-        rows = [False for _ in range(num_rows)]
-        cols = [False for _ in range(num_cols)]
+        
+        first_row = False
+        first_col = False
+        
+        for i in range(num_rows):
+            if matrix[i][0] == 0:
+                first_col = True
+        
+        for j in range(num_cols):
+            if matrix[0][j] == 0:
+                first_row = True
         
         for i in range(num_rows):
             for j in range(num_cols):
                 if matrix[i][j] == 0:
-                    rows[i] = True
-                    cols[j] = True
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
         
-        for i in range(num_rows):
-            for j in range(num_cols):
-                if rows[i] or cols[j]:
+        for i in range(1, num_rows):
+            for j in range(1, num_cols):
+                if matrix[0][j] == 0 or matrix[i][0] == 0:
                     matrix[i][j] = 0
+        
+        if first_col:
+            for i in range(num_rows):
+                matrix[i][0] = 0
+            
+        if first_row:
+            for j in range(num_cols):
+                matrix[0][j] = 0

eb3ca230535fb8b4505e611fae0d5b58145fa177
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-05 16:48:45 +0800

    Time: 38 ms (75.92%), Space: 13.9 MB (7.16%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.py
+++ 190-reverse-bits/190-reverse-bits.py
@@ -1,5 +1,8 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        string = bin(n)[2:]
-        to_pad = 32 - len(string)
-        return int(string[::-1] + '0' * to_pad, 2)
+        n = (n >> 16) | (n << 16)
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8)
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4)
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2)
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1)
+        return n

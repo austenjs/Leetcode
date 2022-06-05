370d53f9fd6ddacf70e33f62dd8c666efed4fe06
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-05 16:13:31 +0800

    Time: 52 ms (39.06%), Space: 13.8 MB (98.04%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,57 +1,10 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        carry = 0
-        stack_a = list(a)
-        stack_b = list(b)
-        ans = []
-        while stack_a and stack_b:
-            first, second = stack_a.pop(), stack_b.pop()
-            if carry:
-                if first == "1" and second == "1":
-                    carry = 1
-                    ans.append("1")
-                elif first == "1" and second == "0":
-                    carry = 1
-                    ans.append("0")
-                elif first == "0" and second == "1":
-                    carry = 1
-                    ans.append("0")
-                else:
-                    carry = 0
-                    ans.append("1")
-            else:
-                if first == "1" and second == "1":
-                    carry = 1
-                    ans.append("0")
-                elif first == "1" and second == "0":
-                    carry = 0
-                    ans.append("1")
-                elif first == "0" and second == "1":
-                    carry = 0
-                    ans.append("1")
-                else:
-                    carry = 0
-                    ans.append("0")
-        while stack_a:
-            item = stack_a.pop()
-            if carry and item == "1":
-                carry = 1
-                ans.append("0")
-            elif carry:
-                carry = 0
-                ans.append("1")
-            else:
-                ans.append(item)
-        while stack_b:
-            item = stack_b.pop()
-            if carry and item == "1":
-                carry = 1
-                ans.append("0")
-            elif carry:
-                carry = 0
-                ans.append("1")
-            else:
-                ans.append(item)
-        if carry:
-            ans.append("1")
-        return "".join(ans[::-1])
+        x, y = int(a, 2), int(b, 2)
+
+        while y:
+            tmp = x ^ y
+            carry = (x & y) << 1
+            x, y = tmp, carry
+        
+        return bin(x)[2:]

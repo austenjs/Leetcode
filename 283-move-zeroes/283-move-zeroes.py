bbc1c7a8a559bf9ba48e52fe5432fb1982aef9a3
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-05 16:39:59 +0800

    Time: 215 ms (60.93%), Space: 15.6 MB (64.30%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,19 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        N = len(nums)
-        index = 0
-        count = 0
+        pos = 0
         
-        for num in nums:
-            if num == 0:
-                count += 1
-        
-        while index < N and count:
-            if nums[index] == 0:
-                nums.append(nums.pop(index))
-                count -= 1
-            else:
-                index += 1
+        for i, num in enumerate(nums):
+            if num != 0:
+                nums[pos], nums[i] = nums[i], nums[pos]
+                pos += 1
+
 
             

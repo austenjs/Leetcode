76cdbb92c6c18ad9356a1be5404e3bbc17c02208
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-06 21:00:08 +0800

    Time: 408 ms (9.97%), Space: 31.5 MB (72.52%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
@@ -1,57 +1,43 @@
+class Trie:
+
+    def __init__(self):
+        self.trienode = TrieNode('')
+
+    def insert(self, word: str) -> None:
+        root = self.trienode
+        for char in word:
+            if char not in root.children:
+                root.children[char] = TrieNode(char)
+            root = root.children[char]
+        root.set_word()
+
+    def search(self, word: str) -> bool:
+        root = self.trienode
+        for char in word:
+            if char not in root.children:
+                return False
+            root = root.children[char]
+        return root.check_word()      
+
+    def startsWith(self, prefix: str) -> bool:
+        root = self.trienode
+        for char in prefix:
+            if char not in root.children:
+                return False
+            root = root.children[char]
+        return True   
+
 class TrieNode:
     def __init__(self, char):
         self.char = char
         self.children = {}
-        self.end = False
-    
-    def is_word(self):
-        return self.end
+        self.is_word = False
+        
+    def check_word(self):
+        return self.is_word
     
     def set_word(self):
-        self.end = True
-    
-    def add_child(self, char, node):
-        self.children[char] = node
-        
-        
-    def is_child(self, char):
-        return char in self.children
-    
-    def get_child(self, char):
-        return self.children[char]
-
-class Trie:
-
-    def __init__(self):
-        self.root = TrieNode("")
-
-    def insert(self, word: str) -> None:
-        node = self.root
-        for char in word:
-            if node.is_child(char):
-                node = node.get_child(char)
-            else:
-                tmp_node = TrieNode(char)
-                node.add_child(char, tmp_node)
-                node = tmp_node
-        node.set_word()
-
-    def search(self, word: str) -> bool:
-        node = self.root
-        for char in word:
-            if not node.is_child(char):
-                return False
-            node = node.get_child(char)
-        return node.is_word()
-
-    def startsWith(self, prefix: str) -> bool:
-        node = self.root
-        for char in prefix:
-            if not node.is_child(char):
-                return False
-            node = node.get_child(char)
-        return True
-
+        self.is_word = True
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()

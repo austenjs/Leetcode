6c216f3ea94ab01271ea9aa42471749746ec70e3
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-08 17:41:36 +0800

    Time: 2678 ms (19.66%), Space: 25.3 MB (16.97%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -3,21 +3,24 @@ class Solution:
         self.memo = {0 : 0}
 
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if amount in self.memo:
-            return self.memo[amount]
-
-        # Base case
-        if amount < min(coins):
-            self.memo[amount] = -1
+        if amount == 0:
+            return 0
+        if min(coins) > amount:
             return -1
         for coin in coins:
-            if coin == amount:
-                self.memo[coin] = 1
-                return 1
+            self.memo[coin] = 1
+        return self.getMinCoins(sorted(coins)[::-1], amount)
+
+    def getMinCoins(self, coins, amount):
+        # Base case
+        if amount < 0:
+            return -1
+        if amount in self.memo:
+            return self.memo[amount]
         
         minimum = 10000
-        for coin in reversed(sorted(coins)):
-            additional_coins = self.coinChange(coins, amount - coin)
+        for coin in coins:
+            additional_coins = self.getMinCoins(coins, amount - coin)
             if additional_coins == -1:
                 continue
             minimum = min(minimum, additional_coins)

f6063fba517a0a6bf22cfe09fc30d277eb36f226
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-08 17:57:16 +0800

    Time: 2652 ms (20.42%), Space: 14.1 MB (64.37%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,10 +1,5 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # coins = [1, 2, 5]
-        # amt = 6
-        # F(5) = 1
-        # min(F(5 - 1), F(5 - 2), F(5 - 5)) + 1
-        # F = [0, 1, 1, 2, 2, 1, 2]
         memo = [1e5 for _ in range(amount + 1)]
         memo[0] = 0
         

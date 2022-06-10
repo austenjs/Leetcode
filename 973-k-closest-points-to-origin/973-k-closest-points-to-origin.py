6481970cdd81b22d17a7a8d6d3baaf9d2d2af170
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-10 08:03:18 +0800

    Time: 1608 ms (8.52%), Space: 20.3 MB (59.04%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,30 +1,7 @@
 class Solution:
-    def calculateDistance(self, point):
-        return point[0] * point[0] + point[1] * point[1]
-    
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        if k == 1:
-            return [min(points, key = lambda point : self.calculateDistance(point))]
-        if len(points) == k:
+        N = len(points)
+        if N == k:
             return points
-        pivot_point = points[len(points) // 2]
-        pivot = self.calculateDistance(pivot_point)
-        left = []
-        right = []
-        for point in points:
-            if point == pivot_point:
-                continue
-            if self.calculateDistance(point) > pivot:
-                right.append(point)
-            else:
-                left.append(point)
-        if len(left) == k:
-            return left
-        elif len(left) == k - 1:
-            left.append(pivot_point)
-            return left
-        elif len(left) < k:
-            left.append(pivot_point)
-            return left + self.kClosest(right, k - len(left))
-        else:
-            return self.kClosest(left, k)
+        sorted_points = sorted(points, key = lambda x : x[0] * x[0] + x[1] * x[1])
+        return sorted_points[:k]

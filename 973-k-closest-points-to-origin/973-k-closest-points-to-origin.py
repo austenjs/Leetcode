e1d75464422b3e4f83c633376007313a9d101340
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-10 08:11:32 +0800

    Time: 805 ms (93.28%), Space: 20.4 MB (37.26%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,7 +1,26 @@
+import random
+
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         N = len(points)
         if N == k:
             return points
-        sorted_points = sorted(points, key = lambda x : x[0] * x[0] + x[1] * x[1])
-        return sorted_points[:k]
+        
+        dist_pivot = self.distance(points[random.randint(0, len(points) - 1)])
+        left = []
+        right = []
+        for point in points:
+            if self.distance(point) <= dist_pivot:
+                left.append(point)
+            else:
+                right.append(point)
+        
+        if len(left) == k:
+            return left
+        elif len(left) < k:
+            return left + self.kClosest(right, k - len(left))
+        else:
+            return self.kClosest(left, k)
+            
+    def distance(self, point):
+        return point[0] * point[0] + point[1] * point[1]

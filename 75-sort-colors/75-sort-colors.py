384ed244b6a0dbaa76ccbd8aa4f4666c531c5efc
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-10 08:22:12 +0800

    Time: 52 ms (37.77%), Space: 13.8 MB (62.80%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,15 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        bins = [0 for _ in range(3)]
-        for col in nums:
-            bins[col] += 1
-        for i in range(len(nums)):
-            if bins[0]:
-                nums[i] = 0
-                bins[0] -= 1
-            elif bins[1]:
-                nums[i] = 1
-                bins[1] -= 1
+        p0 = curr = 0
+        p2 = len(nums) - 1
+
+        while curr <= p2:
+            if nums[curr] == 0:
+                nums[p0], nums[curr] = nums[curr], nums[p0]
+                p0 += 1
+                curr += 1
+            elif nums[curr] == 2:
+                nums[p2], nums[curr] = nums[curr], nums[p2]
+                p2 -= 1
             else:
-                nums[i] = 2
+                curr += 1

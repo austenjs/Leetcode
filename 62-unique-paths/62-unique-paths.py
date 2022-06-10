41ee3a0220bf4dc1919b8c2b3381b68a9b2c0f58
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-11 02:36:41 +0800

    Time: 37 ms (74.72%), Space: 13.9 MB (32.28%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,10 +1,6 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memo = [[0 for _ in range(n)] for _ in range(m)]
-        for i in range(m):
-            memo[i][0] = 1
-        for j in range(n):
-            memo[0][j] = 1
+        memo = [[1 for _ in range(n)] for _ in range(m)]
         
         for i in range(1, m):
             for j in range(1, n):

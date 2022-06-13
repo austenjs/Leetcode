d6d7773530631213fa0ca12daa38236a591e190c
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-13 18:13:28 +0800

    Time: 55 ms, Memory: 13.8 MB - LeetHub

---
+++ powx-n/powx-n.py
@@ -0,0 +1,11 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if n < 0:
+            return self.myPow(1 / x, -n)
+        
+        square = self.myPow(x, int(n / 2))
+        if n % 2:
+            return x * square * square
+        return square * square

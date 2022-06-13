6368916f9620ef715bf8058f941fa704085cb945
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-13 17:24:39 +0800

    Time: 46 ms, Memory: 20.5 MB - LeetHub

--- reverse-linked-list/reverse-linked-list.py
+++ reverse-linked-list/reverse-linked-list.py
@@ -5,17 +5,11 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        return self.reverse(head)[0]
-        
-    def reverse(self, head):
         if head is None or head.next is None:
-            return head, head
-        end = head
-        new_head, new_tail = self.reverse(head.next)
-        end.next = None
-        if new_tail:
-            new_tail.next = end
-        else:
-            new_tail = end
+            return head
         
-        return new_head, end
+        new_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        
+        return new_head

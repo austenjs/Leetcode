b1642f003d8e66c00be44e2c7c990fd81bc32468
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-15 04:03:07 +0800

    Time: 39 ms, Memory: 14 MB - LeetHub

---
+++ height-checker/height-checker.py
@@ -0,0 +1,9 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        expected = sorted(heights)
+        
+        ans = 0
+        for i, num in enumerate(heights):
+            if num != expected[i]:
+                ans += 1
+        return ans

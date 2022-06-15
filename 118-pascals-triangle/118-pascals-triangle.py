b141d5627650d91a1a8c82f1fe7e42b7621c47d8
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-15 14:43:32 +0800

    Time: 39 ms (66.47%), Space: 14 MB (17.19%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,14 +1,12 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        if numRows == 1:
-            return [[1]]
-        ans = [[1]]
-        for index in range(numRows - 1):
-            new_arr = []
-            current = 0
-            for i in range(len(ans[index])):
-                new_arr.append(current + ans[index][i])
-                current = ans[index][i]
-            new_arr.append(1)
-            ans.append(new_arr)
+        ans = []
+        for i in range(numRows):
+            row = [[] for _ in range(i + 1)]
+            row[0] = row[-1] = 1
+            
+            for j in range(1, len(row) - 1):
+                row[j] = ans[i - 1][j - 1] + ans[i - 1][j]
+            ans.append(row)
+            
         return ans

e6cbe7ef7ea7d5ef8eb0b84b11ab94609b30c522
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-15 14:27:14 +0800

    Time: 221 ms, Memory: 15.3 MB - LeetHub

---
+++ find-pivot-index/find-pivot-index.py
@@ -0,0 +1,14 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left, right = 0, sum(nums) - nums[0]
+        if left == right:
+            return 0
+        
+        for i in range(1, len(nums)):
+            left += nums[i - 1]
+            right -= nums[i]
+            if left == right:
+                return i
+        
+        return -1
+            

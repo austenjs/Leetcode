124589ee11fcc00f520d9cad0bf1d06877cd2f49
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-23 02:32:10 +0800

    Time: 405 ms, Memory: 20.3 MB - LeetHub

---
+++ search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        M = len(matrix)
+        N = len(matrix[0])
+        
+        r, c = M - 1, 0
+        
+        while r >= 0 and c < N:
+            if matrix[r][c] == target:
+                return True
+            elif matrix[r][c] < target:
+                c += 1
+            else:
+                r -= 1
+        
+        return False

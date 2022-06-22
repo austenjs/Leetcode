24ae9153429ccad57bddb2cb7f8f45911db952cb
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-23 02:48:49 +0800

    Time: 95 ms (35.95%), Space: 13.9 MB (37.24%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        board = [[False for _ in range(n)] for _ in range(n)]
+        queens = set()
+        
+        def backtrack(row, count):
+            for col in range(n):
+                if not under_attack(row, col):
+                    board[row][col] = True
+                    queens.add((row, col))
+                    if row + 1 == n:
+                        count += 1
+                    else:
+                        count = backtrack(row + 1, count)
+                    queens.remove((row, col))
+            return count
+        
+        def under_attack(row, col):
+            for queen in queens:
+                q_row, q_col = queen
+                # Vertical or Horizontal
+                if q_row == row or q_col == col:
+                    return True
+                if (row - q_row == col - q_col) or (row - q_row + col - q_col == 0):
+                    return True
+            return False
+        
+        count = backtrack(0, 0)
+        return count

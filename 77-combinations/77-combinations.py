eb9d5c1c4522cde5fec3575386f2c0a8da3e393e
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-23 03:10:24 +0800

    Time: 715 ms (25.37%), Space: 15.9 MB (50.17%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -1,15 +1,13 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        return self.generate(1, n, k)
-        
-    def generate(self, start, end, k):
-        if k == 1:
-            return [[i] for i in range(start, end + 1)]
-        
-        ans = []
-        for begin in range(start, end + 1):
-            to_merge = self.generate(begin + 1, end, k - 1)
-            for item in to_merge:
-                ans.append([begin] + item)
-        
-        return ans
+        output = []
+        def backtrack(start = 1, cur = []):
+            if len(cur) == k:
+                output.append(cur.copy())
+                return
+            for i in range(start, n + 1):
+                cur.append(i)
+                backtrack(i + 1, cur)
+                cur.pop()
+        backtrack()
+        return output

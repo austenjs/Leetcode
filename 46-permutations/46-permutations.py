bc0c2cde5fc6179117b5da91d4993d7c914a08f1
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-22 21:10:41 +0800

    Time: 59 ms (50.22%), Space: 14.1 MB (57.59%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,24 +1,15 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        list_of_indices = self.generate_index(len(nums))
-        for indice in list_of_indices:
-            cur = []
-            for index in indice:
-                cur.append(nums[index])
-            ans.append(cur)
-        return ans
-
-    def generate_index(self, N):
-        if N == 1:
-            return [[0]]
-        new_index = N - 1
-        prev_indices = self.generate_index(N - 1)
+        def backtrack(first = 0, depth = 0):
+            if depth == N:
+                output.append(nums.copy())
+                return
+            for i in range(first, N):
+                nums[first], nums[i] = nums[i], nums[first]
+                backtrack(first + 1, depth + 1)
+                nums[first], nums[i] = nums[i], nums[first]
         
-        ans = []
-        for indice in prev_indices:
-            for i in range(len(indice) + 1):
-                new_indice = indice.copy()
-                new_indice.insert(i, new_index)
-                ans.append(new_indice)
-        return ans
+        output = []
+        N = len(nums)
+        backtrack()
+        return output

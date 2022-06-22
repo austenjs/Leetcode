07a957a2ec79f764cd3187e9c3ff8b312b2c8e1e
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-23 04:06:25 +0800

    Time: 103 ms (50.21%), Space: 14.8 MB (72.03%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,17 +1,30 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        pivot = nums[0]
+        if k == 1:
+            return max(nums)
+        elif k == len(nums):
+            return min(nums)
+
+        N = len(nums)
+        pivot = N // 2
         left = []
         right = []
-        for i in range(1, len(nums)):
-            if nums[i] < pivot:
-                left.append(nums[i])
+        for i, num in enumerate(nums):
+            if i == pivot:
+                continue
+            if num <= nums[pivot]:
+                left.append(num)
             else:
-                right.append(nums[i])
+                right.append(num)
+        
+        print(left, right, nums[pivot], k)
+        if len(right) == 0:
+            if k == 1:
+                return nums[pivot]
+            return self.findKthLargest(left, k - 1)
+        
         if len(right) == k - 1:
-            return pivot
+            return nums[pivot]
         elif len(right) >= k:
             return self.findKthLargest(right, k)
         else:

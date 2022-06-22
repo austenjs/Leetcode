ed481f4b910b63a2c1a6353eedc6e795340b4d22
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-22 21:01:11 +0800

    Time: 54 ms (40.05%), Space: 14.1 MB (80.65%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,19 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        nums = sorted(nums)
-        return [[]] + self.non_empty_subset(nums)
-        
-    def non_empty_subset(self, nums):
+        def backtrack(first = 0, curr = []):
+            if len(curr) == k:
+                output.append(curr.copy())
+                return
+            for i in range(first, N):
+                curr.append(nums[i])
+                backtrack(i + 1, curr)
+                curr.pop()
+                
+        output = []
         N = len(nums)
-        if N <= 1:
-            return [nums]
-        subset1 = self.non_empty_subset(nums[:N // 2])
-        subset2 = self.non_empty_subset(nums[N // 2:])
-        return self.combine(subset1, subset2)
-        
-    def combine(self, subset1, subset2):
-        ans = []
-        for i in range(len(subset1)):
-            for j in range(len(subset2)):
-                ans.append(subset1[i] + subset2[j])
-        return subset1 + subset2 + ans
+        for k in range(N + 1):
+            backtrack()
+            
+        return output

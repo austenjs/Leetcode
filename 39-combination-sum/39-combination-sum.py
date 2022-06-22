d3f0f7637e2b01a2ec8943a8e2b69c3f7d265ece
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-23 03:31:09 +0800

    Time: 106 ms (63.54%), Space: 14.2 MB (19.77%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,41 +1,18 @@
-from collections import deque
-
-class Node:
-    def __init__(self, elements):
-        self.total = sum(elements)
-        self.elements = elements
-    
-    def insert(self, element):
-        self.total += element
-        self.elements.append(element)
-
 class Solution:
-    
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        queue = deque()
-        visited = set()
-        ans = []
-        inserted = set()
-        
-        start_node = Node([])
-        queue.append(start_node)
-        
-        while queue:
-            node = queue.popleft()
-            if node.total > target or node in visited:
-                continue
-            if node.total == target:
-                check = tuple(sorted(node.elements))
-                if check in inserted:
-                    continue
-                ans.append(list(check))
-                inserted.add(check)
-                continue
-            visited.add(node)
-            total = node.total
-            elements = node.elements.copy()
-            for candidate in candidates:
-                tmp = elements.copy()
-                tmp.append(candidate)
-                queue.append(Node(tmp))
-        return ans
+        N = len(candidates)
+        def backtrack(start = 0, cur = []):
+            if sum(cur) == target:
+                output.append(cur.copy())
+                return
+            elif sum(cur) > target:
+                return
+            for i in range(start, N):
+                candidate = candidates[i]
+                cur.append(candidate)
+                backtrack(i, cur)
+                cur.pop()
+
+        output = []
+        backtrack()
+        return output

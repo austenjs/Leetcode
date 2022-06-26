81401154b40a2da719e6620ce80cd5b7ea62eb08
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-26 21:21:37 +0800

    Time: 85 ms (91.99%), Space: 17.3 MB (53.77%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,65 +1,28 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         ans = []
-        N = len(intervals)
+        new_start, new_end = newInterval
+        idx, N = 0, len(intervals)
         
-        # Non-overlapping section
-        i = 0
+        # Add intervals that start before newInterval
+        while idx < N and new_start > intervals[idx][0]:
+            ans.append(intervals[idx])
+            idx += 1
         
-        while i < N:
-            cur = intervals[i]
-            if self.is_overlapping(cur, newInterval):
-                break
-            if cur[0] > newInterval[1]:
-                break
-            ans.append(intervals[i])
-            i += 1
-        
-        print(ans, intervals)
-        if i == N:
+        # Add newInterval
+        if not ans or ans[-1][1] < new_start:
             ans.append(newInterval)
-            return ans
-        
-        if i == 0:
-            if len(intervals) == 0:
-                return [newInterval]
-            elif self.is_overlapping(intervals[0], newInterval):
-                combined = self.combine(intervals[0], newInterval)
-                i = 1
-                while i < N and self.is_overlapping(combined, intervals[i]):
-                    combined = self.combine(combined, intervals[i])
-                    i += 1
-                ans.append(combined)
-                while i < N:
-                    ans.append(intervals[i])
-                    i += 1
-                return ans
-            else:
-                ans.append(newInterval)
-                return ans + intervals
-
-        if not self.is_overlapping(cur, newInterval):
-            return intervals[:i] + [newInterval] + intervals[i:]
-        
-        combined = self.combine(intervals[i], newInterval)
-        
-        i += 1
-        while i < N and self.is_overlapping(combined, intervals[i]):
-            combined = self.combine(combined, intervals[i])
-            i += 1
-        ans.append(combined)
-
-        while i < N:
-            ans.append(intervals[i])
-            i += 1
-        
-        return ans
-    
-    def is_overlapping(self, interval1, interval2):
-        if interval1[0] > interval2[0]:
-            return interval1[0] <= interval2[1]
         else:
-            return interval1[1] >= interval2[0]
-    
-    def combine(self, interval1, interval2):
-        return [min(interval1[0], interval2[0]), max(interval1[1], interval2[1])]
+            ans[-1][1] = max(ans[-1][1], new_end)
+        
+        # Add the rest
+        while idx < N:
+            cur = intervals[idx]
+            start, end = cur
+            if ans[-1][1] < start:
+                ans.append(cur)
+            else:
+                ans[-1][1] = max(ans[-1][1], end)
+            idx += 1
+            
+        return ans

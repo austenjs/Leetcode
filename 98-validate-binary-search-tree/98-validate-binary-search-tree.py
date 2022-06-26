949fcbb965879befabec5d60a3def737e384f904
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-26 21:22:31 +0800

    Time: 104 ms (5.10%), Space: 16.6 MB (28.55%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -1,3 +1,4 @@
+import math
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,19 +7,15 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        if root.left and root.val <= root.left.val:
-            return False
-        if root.right and root.val >= root.right.val:
-            return False
-        
-        return self.validate(root.left, None, root.val) and self.validate(root.right, root.val, None)
+        return self.validate(root, -math.inf, math.inf)
         
     def validate(self, node, lower, upper):
         if node is None:
             return True
-        if lower and node.val <= lower:
+        print(node.val, lower, upper)
+        if node.val <= lower:
             return False
-        if upper and node.val >= upper:
+        if node.val >= upper:
             return False
         
         return self.validate(node.left, lower, node.val) and self.validate(node.right, node.val, upper)

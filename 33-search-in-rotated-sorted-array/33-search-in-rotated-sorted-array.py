3f150f1be67e21cc3f7f84d862744bdb8de014a4
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-26 16:58:12 +0800

    Time: 110 ms (5.51%), Space: 14.3 MB (18.79%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,30 +1,23 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         N = len(nums)
-        left = self.find_min_index(nums)
-        right = left + N - 1
+        left = 0
+        right = N - 1
         while left <= right:
             mid = left + (right - left) // 2
-            cur = nums[mid % N]
+            cur = nums[mid]
             if cur == target:
-                return mid % N
-            elif cur > target:
-                right = mid - 1
+                return mid
+            elif cur >= nums[left]:
+                if target >= nums[left] and target < cur:
+                    right = mid - 1
+                else:
+                    left = mid + 1
             else:
-                left = mid + 1
+                if target <= nums[right] and target > cur:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+                
         return -1
     
-    def find_min_index(self, nums : List[int]):
-        N = len(nums)
-        left, right = 0, N - 1
-        while left <= right:
-            mid = left + (right - left) // 2
-            if nums[mid] < nums[mid - 1] and nums[mid] < nums[(mid + 1) % N]:
-                return mid
-            elif nums[mid] > nums[mid - 1] and nums[mid] > nums[(mid + 1) % N]:
-                return (mid + 1) % N
-            elif nums[mid] > nums[-1]:
-                left = mid + 1
-            else:
-                right = mid - 1
-        return left % N

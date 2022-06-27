d9d86fffc8a2ba416a2d2c1993f9ab47ca9dc5e5
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-28 04:40:30 +0800

    Time: 620 ms (53.16%), Space: 26.1 MB (30.01%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,8 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        visited = set()
+        seen = set()
         for num in nums:
-            if num in visited:
+            if num in seen:
                 return True
-            visited.add(num)
+            seen.add(num)
         return False

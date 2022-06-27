8b880c99db35ccb16d2c4ed0f4409f6d50aa27d6
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-28 04:41:39 +0800

    Time: 803 ms (84.89%), Space: 28.7 MB (9.57%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,11 +1,12 @@
+import math
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        ans = nums[0]
-        current = 0
+        cur = 0
+        ans = -math.inf
         for num in nums:
-            current += num
-            if current > ans:
-                ans = current
-            if current < 0:
-                current = 0
+            cur += num
+            ans = max(ans, cur)
+            if cur < 0:
+                cur = 0
         return ans
+        

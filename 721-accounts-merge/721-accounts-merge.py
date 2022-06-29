8e311446870bc3b39e5aac2ede2d87a30caaaa6c
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-29 15:54:55 +0800

    Time: 349 ms (42.14%), Space: 17.7 MB (96.80%) - LeetHub

--- 721-accounts-merge/721-accounts-merge.py
+++ 721-accounts-merge/721-accounts-merge.py
@@ -1,30 +1,29 @@
-class UF:
+class UnionFind:
     def __init__(self, N):
         self.parents = list(range(N))
-
+    
     def union(self, child, parent):
         self.parents[self.find(child)] = self.find(parent)
-
-    def find(self, x):
-        if x != self.parents[x]:
-            self.parents[x] = self.find(self.parents[x])
-        return self.parents[x]
     
+    def find(self, child):
+        if child != self.parents[child]:
+            self.parents[child] = self.find(self.parents[child])
+        return self.parents[child]
+        
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        uf = UF(len(accounts))
+        N = len(accounts)
+        union_find = UnionFind(N)
         
-        # Creat unions between indexes
-        ownership = {}
+        email_to_id = {}
         for i, (_, *emails) in enumerate(accounts):
             for email in emails:
-                if email in ownership:
-                    uf.union(i, ownership[email])
-                ownership[email] = i
-        
-        # Append emails to correct index
+                if email in email_to_id:
+                    union_find.union(i, email_to_id[email])
+                email_to_id[email] = i
+    
         ans = collections.defaultdict(list)
-        for email, owner in ownership.items():
-            ans[uf.find(owner)].append(email)
+        for email, id in email_to_id.items():
+            ans[union_find.find(id)].append(email)
         
-        return [[accounts[i][0]] + sorted(emails) for i, emails in ans.items()]
+        return [[accounts[i][0], *sorted(emails)] for i, emails in ans.items()]

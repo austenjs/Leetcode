c29c490bac9a539a0046b8fc3832c0f1e8b793d6
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-30 00:28:19 +0800

    Time: 42 ms (66.53%), Space: 13.9 MB (65.37%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -3,8 +3,9 @@ class Solution:
         N = len(nums)
         if N == 1:
             return nums[0]
-        dp = [0 for i in range(N)]
+        prev_prev = prev = 0
         
         for i in range(N):
-            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
-        return dp[N - 1]
+            cur = max(prev, prev_prev + nums[i])
+            prev_prev, prev = prev, cur
+        return cur

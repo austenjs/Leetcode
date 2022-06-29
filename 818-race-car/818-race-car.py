1d2d3d2adc3f476c7ef38a451c51a879b9bed057
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-29 12:46:07 +0800

    Time: 48 ms (93.00%), Space: 16 MB (43.58%) - LeetHub

--- 818-race-car/818-race-car.py
+++ 818-race-car/818-race-car.py
@@ -9,16 +9,15 @@ class Solution:
             speed, pos, num_action = queue.popleft()
             if (speed, pos) in visited:
                 continue
-            if pos >= 1.3 * target:
-                continue
             if pos == target:
                 return num_action
             visited.add((speed, pos))
             queue.append((speed * 2, pos + speed, num_action + 1)) # A
-            if speed > 0:
-                new_speed = -1
-            else:
-                new_speed = 1
-            queue.append((new_speed, pos, num_action + 1)) # R
+            
+            #  Only consider changing the direction of the car if one of the following conditions is true
+            #   i.  The car is driving away from the target.
+            #   ii. The car will pass the target in the next move. 
+            if (pos + speed > target and speed > 0) or (pos + speed < target and speed < 0):
+                queue.append((-1 if speed > 0 else 1, pos, num_action + 1))
         
         return -1

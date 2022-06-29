49750468f74a88331ffdcafc5434362f007c89a0
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-29 16:22:45 +0800

    Time: 776 ms (70.55%), Space: 16.2 MB (53.38%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -7,17 +7,16 @@ class Solution:
         target = total // 2
         if target in nums:
             return True
-    
-        N = len(nums)
-        dp = [[False for _ in range(total + 1)] for _ in range(N + 1)]
-        dp[0][0] = True
-        for i in range(1, N + 1):
-            cur = nums[i - 1]
-            for j in range(target + 1):
-                if j < cur:
-                    dp[i][j] = dp[i - 1][j]
-                else:
-                    dp[i][j] = dp[i - 1][j - cur] or dp[i - 1][j]
-        return dp[N][target]
+        
+        dp = set([0])
+        for num in reversed(nums):
+            new_dp = set()
+            for t in dp:
+                if t + num == target:
+                    return True
+                new_dp.add(t + num)
+            dp = dp.union(new_dp)
+        
+        return target in dp
         
         

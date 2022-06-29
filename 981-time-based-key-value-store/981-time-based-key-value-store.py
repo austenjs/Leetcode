e96cd07efffe2db65f821f2422f77896e669b498
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-29 14:09:05 +0800

    Time: 787 ms (87.34%), Space: 68.3 MB (95.21%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.py
+++ 981-time-based-key-value-store/981-time-based-key-value-store.py
@@ -1,29 +1,14 @@
-from collections import defaultdict, OrderedDict
 class TimeMap:
-
     def __init__(self):
-        self.storage = {}
+        self.meta = collections.defaultdict(list)
+        self.data = collections.defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.storage:
-            self.storage[key] = {}
-        self.storage[key][timestamp] = value
+        self.meta[key].append(timestamp)
+        self.data[key].append(value)
 
     def get(self, key: str, timestamp: int) -> str:
-        if key not in self.storage:
-            return ""
-        time = timestamp
-        while time:
-            if time in self.storage[key]:
-                ans = self.storage[key][time]
-                for t in range(time, timestamp + 1):
-                    self.storage[key][time] = ans
-                return ans
-            time -= 1
-        return ""
-                
-
-# Your TimeMap object will be instantiated and called as such:
-# obj = TimeMap()
-# obj.set(key,value,timestamp)
-# param_2 = obj.get(key,timestamp)
+        idx = bisect.bisect(self.meta[key], timestamp)
+        if idx == 0:
+            return ''
+        return self.data[key][idx - 1]

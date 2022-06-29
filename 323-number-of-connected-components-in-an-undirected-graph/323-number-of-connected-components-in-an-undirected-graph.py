e784ed40b5b398fe358bf193e7f42b9c7d1f8a36
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-30 01:04:15 +0800

    Time: 232 ms (10.81%), Space: 15.4 MB (86.00%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -2,9 +2,20 @@ from collections import deque, defaultdict
 class UnionFind:
     def __init__(self, N):
         self.parents = list(range(N))
+        self.ranks = [0 for _ in range(N)]
 
-    def union(self, child, parent):
-        self.parents[self.find(child)] = self.find(parent)
+    def union(self, x, y):
+        parent_x = self.find(x)
+        parent_y = self.find(y)
+        if parent_x == parent_y:
+            return
+        if self.ranks[parent_x] > self.ranks[parent_y]:
+            self.parents[parent_y] = self.parents[parent_x]
+        elif self.ranks[parent_x] < self.ranks[parent_y]:
+            self.parents[parent_x] = self.parents[parent_y]
+        else:
+            self.parents[parent_x] = self.parents[parent_y]
+            self.ranks[parent_y] += 1
 
     def find(self, child):
         if child != self.parents[child]:

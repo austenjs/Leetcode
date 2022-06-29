e21ae79d98cf3d15883ae687045c53bf42700253
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-29 11:34:43 +0800

    Time: 1856 ms (21.33%), Space: 25.1 MB (37.72%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,15 +1,11 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        low_value = 9999
-        profit = 0
-        for i, num in enumerate(prices):
-            if num < low_value:
-                low_value = num
-            new_profit = num - low_value
-            if new_profit > profit:
-                profit = new_profit
-        return profit
+import math
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        ans = 0
+        cur_min = math.inf
+        for price in prices:
+            if price < cur_min:
+                cur_min = price
+            ans = max(price - cur_min, ans)
+        return ans

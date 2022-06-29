f9fb3ccff9ba547ff4989f0dd43b70e256ef7fda
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-30 00:42:09 +0800

    Time: 936 ms (65.63%), Space: 16.3 MB (53.19%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -1,22 +1,15 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         total = sum(nums)
-        if total % 2 != 0:
+        if total % 2 == 1:
             return False
-
+        
         target = total // 2
-        if target in nums:
-            return True
-        
-        dp = set([0])
-        for num in reversed(nums):
-            new_dp = set()
-            for t in dp:
-                if t + num == target:
-                    return True
-                new_dp.add(t + num)
-            dp = dp.union(new_dp)
-        
-        return target in dp
-        
-        
+        possible_sums = set([0])
+        for num in nums:
+            new_sums = set()
+            for t in possible_sums:
+                new_sums.add(t + num)
+            possible_sums = possible_sums.union(new_sums)
+            
+        return target in possible_sums

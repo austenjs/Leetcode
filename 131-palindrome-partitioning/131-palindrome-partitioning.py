b923763c09b9cdb62c6a9a4855e6dd64098e8b72
austenjs <64929507+austenjs@users.noreply.github.com>
2022-06-29 21:30:19 +0800

    Time: 701 ms (87.28%), Space: 30.4 MB (26.31%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -1,45 +1,24 @@
 class Solution:
-    def __init__(self):
-        self.palindrome_list = set()
-        self.str_to_list = defaultdict()
-
     def partition(self, s: str) -> List[List[str]]:
-        if s in self.str_to_list:
-            return self.str_to_list[s]
-
         N = len(s)
-        if N == 0:
-            return [[]]
-        if N == 1:
-            return [[s]]
-        if N == 2:
-            ans = [[s[0], s[1]]]
-            if s[0] == s[1]:
-                ans.append([s])
-            self.str_to_list[s] = ans
-            return ans
-        unique = set()
-        for i in range(1, N):
-            left = self.partition(s[:i])
-            right = self.partition(s[i:])
-            for l in left:
-                for r in right:
-                    unique.add(tuple(l + r))
-        ans = [[*items] for items in unique]
+        def backtrack(start = 0, cur = []):
+            if start == N:
+                ans.append(cur.copy())
+                return
+            for end in range(start, N):
+                if self.is_palindrome(s, start, end):
+                    cur.append(s[start:end + 1])
+                    backtrack(end + 1, cur)
+                    cur.pop()
         
-        if self.is_palindrome(s):
-            ans.append([s])
-        self.str_to_list[s] = ans
+        ans = []
+        backtrack()
         return ans
     
-    def is_palindrome(self, s):
-        if s in self.palindrome_list:
-            return True
-        left, right = 0, len(s) - 1
+    def is_palindrome(self, s, left, right):
         while left <= right:
             if s[left] != s[right]:
                 return False
             left += 1
             right -= 1
-        self.palindrome_list.add(s)
         return True

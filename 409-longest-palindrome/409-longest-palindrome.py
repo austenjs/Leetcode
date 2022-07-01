0f9e931a05f7709b14966663a9bceb8ca7e54d1d
austenjs <64929507+austenjs@users.noreply.github.com>
2022-07-01 16:39:12 +0800

    Time: 61 ms (23.94%), Space: 13.8 MB (65.67%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -2,13 +2,15 @@ from collections import Counter
 
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        has_odd = False
-        ans = 0
         count = Counter(s)
+        ans = 0
+        exist_single_char = False
+        
         for val in count.values():
-            if val % 2:
-                has_odd = True
+            if val % 2 == 1:
+                exist_single_char = True
                 ans += val - 1
             else:
                 ans += val
-        return ans + has_odd
+        
+        return ans + exist_single_char

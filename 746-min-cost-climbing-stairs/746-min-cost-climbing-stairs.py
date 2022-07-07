4d7b6e6dfe05641432133dedf4f0120180484b3a
austenjs <64929507+austenjs@users.noreply.github.com>
2022-07-07 10:58:24 +0800

    Time: 69 ms (80.56%), Space: 14.1 MB (44.65%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,8 +1,15 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        if n == 2:
-            return min(cost)
-        for i in range(n - 3, -1, -1):
-            cost[i] += min(cost[i + 1], cost[i + 2])
-        return min(cost[0], cost[1])
+        N = len(cost)
+        
+        dp = [0 for _ in range(N + 1)]
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+
+        for i in range(2, N + 1):
+            if i == N:
+                dp[i] = min(dp[i - 1], dp[i - 2])
+            else:
+                dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i]
+        
+        return dp[N]

4cbc9d39415c1c3c11a6a71b41b814b221316bc6
austenjs <64929507+austenjs@users.noreply.github.com>
2022-07-09 17:32:04 +0800

    Time: 58 ms (23.25%), Space: 13.7 MB (99.73%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -1,7 +1,9 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         ans = 0
-        while n:
-            ans += n % 2
-            n //= 2
+        mask = 1
+        for i in range(32):
+            if (n & mask):
+                ans += 1
+            mask = mask << 1
         return ans
